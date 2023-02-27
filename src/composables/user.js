936de0821ad67c73f1317cb464bb017203c92e13
roomdada <dsieroger@gmail.com>
2023-02-27 10:34:40 +0000

    feat: provider service table

---
+++ src/composables/user.js
@@ -0,0 +1,27 @@
+import { ref } from 'vue'
+import httpClicent from '../axios';
+export default function useUser() {
+
+  const loading = ref(false)
+  const services = ref({});
+  const error = ref(null);
+
+  const userServices = async () => {
+    loading.value = true;
+    await httpClicent.get('user/courses').then((resp) => {
+      if(resp.status){
+        services.value = resp.data.data
+        console.log(services.value)
+        return categories
+      }
+    }).catch((err) => {
+      if(! err.status ){
+        error.value = "Une erreur est survenue";
+      }
+    }).finally(() => {
+      loading.value = false;
+    })
+  }
+
+  return { userServices, error, loading, services}
+}
--- src/views/Dashboard/Index.vue
+++ src/views/Dashboard/Index.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { useStatsStore } from '../../stores/stats';
 import { RouterLink } from 'vue-router';
+import useUser from '../../composables/user';
+
+const { userServices, error, loading, services } = useUser();
 const stats = useStatsStore();
 
+userServices()
 </script>
 <template>
  <div class="page-wrapper">
@@ -139,7 +143,7 @@ const stats = useStatsStore();
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h3 class="card-title">Historique de mes prestatations</h3>
+                    <h3 class="card-title">Mes prestations({{  services?.length }})</h3>
                   </div>
                   <div class="card-body border-bottom py-3">
                     <div class="d-flex">
@@ -162,38 +166,23 @@ const stats = useStatsStore();
                     <table class="table card-table table-vcenter text-nowrap datatable">
                       <thead>
                         <tr>
-                          <th class="w-1"><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices"></th>
-                          <th class="w-1">No. <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="6 15 12 9 18 15" /></svg>
-                          </th>
-                          <th>Invoice Subject</th>
-                          <th>Client</th>
-                          <th>VAT No.</th>
-                          <th>Created</th>
-                          <th>Status</th>
-                          <th>Price</th>
+
+                          <th>Date de création</th>
+                          <th>Libellé</th>
+                          <th>Categorie</th>
                           <th></th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-                          <td><span class="text-muted">001401</span></td>
-                          <td><a href="invoice.html" class="text-reset" tabindex="-1">Design Works</a></td>
+                        <tr v-for='service in services' v-bind:key="service.id">
+                          <td><span class="text-muted">{{ service.created_at }}</span></td>
+                          <td><a href="invoice.html" class="text-reset" tabindex="-1">{{ service.name }}</a></td>
                           <td>
                             <span class="flag flag-country-us"></span>
-                            Carlson Limited
+                           {{ service.category.name }}
                           </td>
-                          <td>
-                            87956621
-                          </td>
-                          <td>
-                            15 Dec 2017
-                          </td>
-                          <td>
-                            <span class="badge bg-success me-1"></span> Paid
-                          </td>
-                          <td>$887</td>
+
+
                           <td class="text-end">
                             <span class="dropdown">
                               <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
@@ -208,33 +197,8 @@ const stats = useStatsStore();
                             </span>
                           </td>
                         </tr>
-
-
                       </tbody>
                     </table>
-                  </div>
-                  <div class="card-footer d-flex align-items-center">
-                    <p class="m-0 text-muted">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
-                    <ul class="pagination m-0 ms-auto">
-                      <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                          <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="15 6 9 12 15 18" /></svg>
-                          prev
-                        </a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">
-                          next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 6 15 12 9 18" /></svg>
-                        </a>
-                      </li>
-                    </ul>
                   </div>
                 </div>
               </div>
